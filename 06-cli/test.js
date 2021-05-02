0ecf8b1bdee00e95b832a2b1ad6f69a4846ccb48
wslmacieira <wslmacieira@outlook.com>
2021-05-01 22:10:46 -0300

    Manipulando arquivos - DELETE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -12,6 +12,7 @@ describe('Suite de manipulação de Heoris', () => {
   before(async () => {
     await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
   });
+
   it('deve pesquisar um heroi usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     const [resultado] = await database.listar(expected.id); // = const posicaoUm = resultado[0];
@@ -22,6 +23,12 @@ describe('Suite de manipulação de Heoris', () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
     const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
-    ok(atual, expected);
+    deepStrictEqual(atual, expected);
+  });
+
+  it('deve remover um heroi por id', async () => {
+    const expected = true;
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepStrictEqual(resultado, expected);
   });
 });
