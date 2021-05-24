15b6c272d63839322cd0f15e79ff1e691027458f
wslmacieira <wslmacieira@outlook.com>
2021-05-24 19:11:23 -0300

    Conhecendo operadores do MongoDB

---
+++ 09-multi-db-mongo/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: 'wslmacieira',
+  password: 'gostack',
+  database: 'heroes',
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+}
---
+++ 09-multi-db-mongo/src/db/strategies/base/contextStrategy.js
@@ -0,0 +1,31 @@
+const ICrud = require('./../interfaces/ICrud');
+
+class ContextStrategy extends ICrud {
+  constructor(strategy) {
+    super();
+    this._database = strategy;
+  }
+
+  create(item) {
+    return this._database.create(item);
+  }
+
+  read(item) {
+    return this._database.read(item);
+  }
+
+  update(id, item) {
+    return this._database.update(id, item);
+  }
+
+  delete(id) {
+    return this._database.delete(id);
+  }
+
+  isConnected() {
+    return this._database.isConnected();
+  }
+
+}
+
+module.exports = ContextStrategy;
